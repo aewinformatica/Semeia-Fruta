7acb848b94b21b618c3f7e05a752b45d41d825b4
Sgt DE JESUS <1109888279@rede.eme>
2017-12-26 14:43:57 -0200

    SemeiaApplication

---
+++ src/main/java/com/aewinformatica/projeto/SemeiaApplication.java
@@ -0,0 +1,12 @@
+package com.aewinformatica.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemeiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemeiaApplication.class, args);
+	}
+}
