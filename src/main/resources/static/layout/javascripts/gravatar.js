4f186082b21f72d48e7e3d48e8de4094f2cf6bb0
aewinformatica <aewinformatica@gmail.com>
2020-07-08 00:42:47 -0300

    Inclusao allowPublicKeyRetrieval=true, condicional gravatar e validacao

--- src/main/resources/static/layout/javascripts/gravatar.js
+++ src/main/resources/static/layout/javascripts/gravatar.js
@@ -1,7 +1,10 @@
 var urlBasica = "https://www.gravatar.com/avatar/";
 var elementEmail = document.querySelector('p.class-email');
-var email = elementEmail.innerHTML;
-var urlFinal = urlBasica +  md5(email);
+if(elementEmail !== null){
+	var email = elementEmail.innerHTML;
+	var urlFinal = urlBasica +  md5(email);
 
-var elementIMG = document.querySelector("img.aw-logged-user__picture");
-	elementIMG.setAttribute("src",urlFinal);
+	var elementIMG = document.querySelector("img.aw-logged-user__picture");
+		elementIMG.setAttribute("src",urlFinal);
+}
+
